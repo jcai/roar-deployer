1a06c3a77a3f06efdf21fbd1a6e4229ec198947c
Jun Tsai <jcai@ganshane.com>
2016-11-15 18:08:12 +0800

    调整分支询问模式

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'roar'
 #set :repo_url, 'roar@git.roar:/opt/apps/roar_deployer.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/opt/apps/roar'
